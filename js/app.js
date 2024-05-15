a7202836d45084859a9e2e8ec860d3c04be28b52
andayanda <carmelaespinola@gmail.com>
2024-05-15 12:55:05 +0200

    boton de ir arriba

--- js/app.js
+++ js/app.js
@@ -9,3 +9,12 @@ function idioma(){
         location.href="../index.html";
     }
 }
+//scroll up
+document.getElementById("button-up").addEventListener("click", scrollUp);
+function scrollUp(){
+    var currentScroll = document.documentElement.scrollTop;
+    if (currentScroll > 0){
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo (0, currentScroll - (currentScroll/ 10));
+    }
+}
