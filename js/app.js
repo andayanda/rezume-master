644fe2cd462d4e92748896062381e4a67750f640
andayanda <carmelaespinola@gmail.com>
2024-05-29 12:02:13 +0200

    hacer que el boton de scroll up se esconda

--- js/app.js
+++ js/app.js
@@ -18,3 +18,13 @@ function scrollUp(){
         window.scrollTo (0, currentScroll - (currentScroll/ 10));
     }
 }
+buttonUp =document.getElementById("button-up");
+window.onscroll = function(){
+    var scroll = document.documentElement.scrollTop;
+    if (scroll >500){
+        buttonUp.style.transform = "scale(1)";        
+    }else if (scroll < 500){
+        buttonUp.style.transform ="scale(0)";
+    }
+
+}
